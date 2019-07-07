e1f418f2fbcc0fcc878e921f44ece472c72605a8
tori <hij0359@naver.com>
2019-07-07 20:38:16 +0900

    강사dao 일부 수정

--- LMS6jo/src/com/bit/lms/model/dao/AssignDao.java
+++ LMS6jo/src/com/bit/lms/model/dao/AssignDao.java
@@ -2,29 +2,29 @@ package com.bit.lms.model.dao;
 
 public class AssignDao {
 	
-	//과제목록
+	//과제목록 - 강사
 	public void assignList(){
 		
 	}
 	
-	//과제상세목록
+	//과제상세목록 - 강사
 	public void assignDetail(){
 		
 	}
 	
-	//과제등록
+	//과제등록 - 수강생
 	public int assignInsert(){
 		
 		return 0;
 	}
 	
-	//과제수정
+	//과제수정 - 수강생
 	public int assignUpdate(){
 		
 		return 0;
 	}
 	
-	//과제삭제
+	//과제삭제 - 수강생
 	public int deleteAssign(){
 		
 		return 0;
